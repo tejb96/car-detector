2c758630c1d2f03eda9c2613170c860e2eff6552
Tejpreet Bal <tejpreetbal@gmail.com>
2023-05-18 14:34:52 -0600

    refactor: rm unnecessary code and added comments

--- input_processing.py
+++ input_processing.py
@@ -19,18 +19,20 @@ class Sensor:
     def __init__(
         self, traffic_light="green", pedestrian_status="no", vehicle_status="no"
     ):
+        # Assign default values to instance variables
         self.traffic_light = traffic_light
         self.pedestrian_status = pedestrian_status
         self.vehicle_status = vehicle_status
 
     # updates the instance variables based on the user input
-    # change_menu_input is the selection of light, pedestrian...and change_occured is the status
+    # change_menu_input is the menu input, and change_occured is the change such as red for light
     def update_status(self, change_menu_input, change_occured):
         # Nested if loops that return the updated status based on the two strings the user entered
-        # The outer if-else loop runs based on the type of change (lights, vehicle...) that was selected by the user
+        # The outer if-else loop runs based on the menu selection (lights, vehicle...)
         if change_menu_input == "1":
             # The inner loop has conditions based on what the change is (light turned red or other)
             if change_occured == "red":
+                # The respective instance variable is pointed to the new value
                 self.traffic_light = change_occured
                 # The updated status is then returned
                 return [self.traffic_light, self.pedestrian_status, self.vehicle_status]
@@ -40,7 +42,7 @@ class Sensor:
             elif change_occured == "yellow":
                 self.traffic_light = change_occured
                 return [self.traffic_light, self.pedestrian_status, self.vehicle_status]
-
+        # If pedestrian is selected
         elif change_menu_input == "2":
             if change_occured == "yes":
                 self.pedestrian_status = change_occured
@@ -48,7 +50,7 @@ class Sensor:
             elif change_occured == "no":
                 self.pedestrian_status = change_occured
                 return [self.traffic_light, self.pedestrian_status, self.vehicle_status]
-
+        # If vehicle is selected
         elif change_menu_input == "3":
             if change_occured == "yes":
                 self.vehicle_status = change_occured
@@ -57,25 +59,27 @@ class Sensor:
                 self.vehicle_status = change_occured
                 return [self.traffic_light, self.pedestrian_status, self.vehicle_status]
 
-        else:
-            return [self.traffic_light, self.pedestrian_status, self.vehicle_status]
-
 
 # The sensor object should be passed to this function to print the action message and current status
 # After inquiring about the change, the status is updated, printed and then reset to default
 def print_message(sensor):
+    # If the input is invalid, the code will run without updating current_status
     current_status = [
         sensor.traffic_light,
         sensor.pedestrian_status,
         sensor.vehicle_status,
     ]
+    # Infinite loop only broken when user enters 0
     while True:
+        # try block
         try:
+            # Asks user to select from the menu and assigns input to change_menu_input as a string
             change_menu_input = input(
                 "Are changes detected in the vision input?"
                 + "\n"
                 + "Select 1 for light, 2 for pedestrian, 3 for vehicle, or 0 to end the program: "
             )
+            # If the user enteres 0, the program is terminated using break
             if change_menu_input == "0":
                 break
 
@@ -84,16 +88,16 @@ def print_message(sensor):
                 # Raises the value error
                 raise ValueError("You must select either 1, 2, 3, 0")
 
-            # Asks what the change is
+            # Asks what the change is, and change_occured is assigned the input
             change_occured = input("What change has been identified?: ")
-            # Checks if the input is valid
+            # If the input is invalid, the message is displayed and the status is not updated
             if change_occured not in ["red", "yellow", "green", "yes", "no"]:
                 print("Invalid vision change.")
             else:
                 # updates the instance variables and returns the current status
                 current_status = sensor.update_status(change_menu_input, change_occured)
 
-            # If lights are red or there is a passenger or vehicle
+            # If lights are red or there is a pedestrian or vehicle
             if (
                 current_status[0] == "red"
                 or current_status[1] == "yes"
@@ -104,39 +108,40 @@ def print_message(sensor):
                     "Light = "
                     + current_status[0]
                     + ", Pedestrian = "
-                    + current_status[1]
+                    + current_status[1]  # Prints the current status
                     + ", Vehicle = "
                     + current_status[2]
                     + "\n"
-                )  # Prints the current status
-            elif current_status[0] == "yellow":  # If yellow
-                print("\n" + "Caution" + "\n")
+                )
+            # If light is yellow
+            elif current_status[0] == "yellow":
+                print("\n" + "Caution" + "\n")  # Prints action message
                 print(
                     "Light = "
                     + current_status[0]
-                    + ", Pedestrian = "
+                    + ", Pedestrian = "  # Prints current status
                     + current_status[1]
                     + ", Vehicle = "
                     + current_status[2]
                     + "\n"
                 )
-            # If green or no pedestrian or no vehicle
+            # If light is green and no pedestrian and no vehicle
             elif (
                 current_status[0] == "green"
-                or current_status[1] == "no"
-                or current_status[2] == "no"
+                and current_status[1] == "no"
+                and current_status[2] == "no"
             ):
-                print("\n" + "Proceed" + "\n")
+                print("\n" + "Proceed" + "\n")  # Prints action message
                 print(
                     "Light = "
                     + current_status[0]
-                    + ", Pedestrian = "
+                    + ", Pedestrian = "  # Prints current status
                     + current_status[1]
                     + ", Vehicle = "
                     + current_status[2]
                     + "\n"
                 )
-
+        # Except block with if-elif for invalid menu selection and invalid change input
         except ValueError as value_error:
             if str(value_error) == "Invalid vision change.":
                 print()
